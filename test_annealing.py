7bb848fd4c08d1cb31a1e31f069b0cf38185ea5c
Petar <sarajcev@users.noreply.github.com>
2023-09-19 11:47:03 +0200

    Initial commit.

---
+++ test_annealing.py
@@ -0,0 +1,13 @@
+from annealing import simulated_annealing
+from numpy import testing, array
+
+def test_annealing():
+    def f(x, y):
+        y = (x-1)**2 + (y+1)**2
+        return y
+    
+    x, e = simulated_annealing(f, [4, 4])
+
+    s = array([1, -1])
+    
+    testing.assert_array_almost_equal(x, s, decimal=1)
